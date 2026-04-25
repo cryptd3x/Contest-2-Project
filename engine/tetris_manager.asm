36f6659469ce4a7e060b1a5b4ad76598e87cc635
Justin Bower <jbower2024@my.fit.edu>
2026-04-24 22:42:51 -0400

    Update tetris_manager.asm
    added header for file + finalize version  1 of file

--- engine/tetris_manager.asm
+++ engine/tetris_manager.asm
@@ -1,3 +1,13 @@
+INCLUDE default_header.inc
+INCLUDE tetris_manager.inc
+INCLUDE heap_functions.inc
+INCLUDE tetromino.inc
+
+.data
+TETRIS_MANAGER_VTABLE GameObject_vtable <OFFSET game_object_start, OFFSET tetris_manager_update, OFFSET game_object_exit, OFFSET free_game_object>
+
+.code
+
 init_tetris_manager PROC PUBLIC USES ecx
 	INVOKE init_game_object, 1
 	mov (GameObject PTR [ecx]).gameObjectType, TETRIS_MANAGER_GAME_OBJECT_ID
