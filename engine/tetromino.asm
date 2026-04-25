d8a509a1bbae0dc76ff4e08fa1ad150d84aefd32
Justin Bower <jbower2024@my.fit.edu>
2026-04-24 22:37:49 -0400

    Update tetromino.asm
    added more functions for tetris pieces

--- engine/tetromino.asm
+++ engine/tetromino.asm
@@ -16,4 +16,25 @@ init_tetromino PROC PUBLIC USES esi, ecx
 	INVOKE new_rect_component, 4, 4, 0, 255, 0, 255
 	INVOKE add_component, ecx, eax
 	ret
+
 init_tetromino ENDP
+INVOKE init_game_object, 2
+	mov (GameObject PTR [ecx]).gameObjectType, TETROMINO_GAME_OBJECT_ID
+	mov (GameObject PTR [ecx]).pVt, OFFSET TETROMINO_VTABLE
+	INVOKE new_transform_component, 40, 5, 0
+	INVOKE add_component, ecx, eax
+	INVOKE new_rect_component, 4, 4, 0, 255, 0, 255
+	INVOKE add_component, ecx, eax
+	ret
+init_tetromino ENDP
+
+new_tetromino PROC PUBLIC USES ecx
+	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF Tetromino
+	mov ecx, eax
+	INVOKE init_tetromino
+	ret
+new_tetromino ENDP
+
+tetromino_update PROC stdcall PUBLIC USES ecx, deltaTime:REAL4
+	ret
+tetromino_update ENDP
