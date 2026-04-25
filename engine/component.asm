5533f6d5aa41c775de393ac4d5039e344c634ebb
Justin Bower <jbower2024@my.fit.edu>
2026-04-24 21:05:56 -0400

    Create component.asm
    Generalized version of file from Contest 1:
    
    Contains component handling functions

---
+++ engine/component.asm
@@ -0,0 +1,18 @@
+INCLUDE default_header.inc
+INCLUDE heap_functions.inc
+INCLUDE component.inc
+
+.data
+COMPONENT_VTABLE Component_vtable <OFFSET free_component>
+
+.code
+init_component PROC PUBLIC USES esi
+	mov (Component PTR [ecx]).componentType, DEFAULT_COMPONENT_ID
+	mov (Component PTR [ecx]).pVt, OFFSET COMPONENT_VTABLE
+	ret
+init_component ENDP
+
+free_component PROC PUBLIC
+	INVOKE HeapFree, hHeap, 0, ecx
+	ret
+free_component ENDP
