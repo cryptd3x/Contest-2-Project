68066d35dbf79a36be052da443834992f5eaafe5
Justin Bower <jbower2024@my.fit.edu>
2026-04-24 21:41:23 -0400

    Create input_manager.asm
    Legacy file from Contest 1:
    
    handles game inputs

---
+++ engine/input_manager.asm
@@ -0,0 +1,20 @@
+; // ==================================
+; // input_manager.asm
+; // ----------------------------------
+; // The input manager is responsible for determining
+; // which keys are currently pressed and providing
+; // a convenient method for other files to determine
+; // the currently pressed keys. This should be
+; // updated on a by-frame basis by a scene.
+; // ==================================
+
+INCLUDE default_header.inc
+INCLUDE input_manager.inc
+
+; // This is a Win32 API function that was added to the program out of need, 
+; // although we did not learn about it in class. The reason this function is present
+; // is because it allows for a "real-time" input system where the hardware is polled
+; // at the instant the frame is updated to determine what keys are currently being
+; // pressed. Because the function is asynchronous, we can call it at any time and it
+; // will reliably spit out the exact keys being pressed.
+; // Documentation used: learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getasynckeystate
