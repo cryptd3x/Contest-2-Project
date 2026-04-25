803ab97eabcc8fecd4b42f82f704e3690dd8641b
Justin Bower <jbower2024@my.fit.edu>
2026-04-24 21:54:59 -0400

    Create game_object.asm
    new barebones version of game object logic file

---
+++ engine/game_object.asm
@@ -0,0 +1,19 @@
+.data
+GAMEOBJECT_VTABLE GameObject_vtable <OFFSET game_object_start, OFFSET game_object_update, OFFSET game_object_exit, OFFSET free_game_object>
+
+.code
+init_game_object PROC PUBLIC USES esi, maxComponents:DWORD
+	mov (GameObject PTR [ecx]).gameObjectType, DEFAULT_GAME_OBJECT_ID
+	mov (GameObject PTR [ecx]).pVt, OFFSET GAMEOBJECT_VTABLE
+	mov (GameObject PTR [ecx]).awaitingFree, 0
+	lea ecx, (GameObject PTR [ecx]).components
+	INVOKE init_unordered_vector, maxComponents
+	ret
+init_game_object ENDP
+
+new_game_object PROC PUBLIC USES ecx, maxComponents:DWORD
+	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF GameObject
+	mov ecx, eax
+	INVOKE init_game_object, maxComponents
+	ret
+new_game_object ENDP
