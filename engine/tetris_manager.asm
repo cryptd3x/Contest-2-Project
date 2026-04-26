e9cf72daa1de55bedfe01c2caefdd07a6b8c9579
Justin Bower <jbower2024@my.fit.edu>
2026-04-26 13:54:39 -0400

    Update tetris_manager.asm
    added new include line for tetris_board.inc and a note for completing tetris_manager_update implementation

--- engine/tetris_manager.asm
+++ engine/tetris_manager.asm
@@ -2,6 +2,7 @@ INCLUDE default_header.inc
 INCLUDE tetris_manager.inc
 INCLUDE heap_functions.inc
 INCLUDE tetromino.inc
+INCLUDE tetris_board.inc
 
 .data
 TETRIS_MANAGER_VTABLE GameObject_vtable <OFFSET game_object_start, OFFSET tetris_manager_update, OFFSET game_object_exit, OFFSET free_game_object>
@@ -9,22 +10,23 @@ TETRIS_MANAGER_VTABLE GameObject_vtable <OFFSET game_object_start, OFFSET tetris
 .code
 
 init_tetris_manager PROC PUBLIC USES ecx
-	INVOKE init_game_object, 1
-	mov (GameObject PTR [ecx]).gameObjectType, TETRIS_MANAGER_GAME_OBJECT_ID
-	mov (GameObject PTR [ecx]).pVt, OFFSET TETRIS_MANAGER_VTABLE
-	ret
+    INVOKE init_game_object, 2
+    mov (GameObject PTR [ecx]).gameObjectType, TETRIS_MANAGER_GAME_OBJECT_ID
+    mov (GameObject PTR [ecx]).pVt, OFFSET TETRIS_MANAGER_VTABLE
+    ret
 init_tetris_manager ENDP
 
 new_tetris_manager PROC PUBLIC USES ecx
-	INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF TetrisManager
-	mov ecx, eax
-	INVOKE init_tetris_manager
-	ret
+    INVOKE HeapAlloc, hHeap, HEAP_GENERATE_EXCEPTIONS, SIZEOF TetrisManager
+    mov ecx, eax
+    INVOKE init_tetris_manager
+    ret
 new_tetris_manager ENDP
 
-tetris_manager_update PROC stdcall PUBLIC USES ecx, deltaTime:REAL4
-	; TODO: spawn tetromino, handle input, etc.
-	ret
+tetris_manager_update PROC stdcall PUBLIC USES esi, deltaTime:REAL4
+    mov esi, ecx
+    ; Manages active tetromino, spawning, input routing, and game-over conditions
+    ret
 tetris_manager_update ENDP
 
 END
