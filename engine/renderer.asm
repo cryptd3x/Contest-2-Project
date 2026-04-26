37af170578194de668e0f0241d2b42c2c6efee99
Justin Bower <jbower2024@my.fit.edu>
2026-04-26 13:58:58 -0400

    Update renderer.asm
    added notes for improved implementation of renderCommands

--- engine/renderer.asm
+++ engine/renderer.asm
@@ -3,18 +3,22 @@ INCLUDE renderer.inc
 INCLUDE engine_types.inc
 INCLUDE render_command.inc
 INCLUDE camera.inc
+
 .data
 screenBuffer Pixel SCREEN_WIDTH * SCREEN_HEIGHT DUP(<0,0,0,255>)
 
 .code
 renderCommands PROC PUBLIC USES esi edi ebx, pRenderCommands:DWORD, numCommands:DWORD, pCamera:DWORD
-	; TODO: full implementation later. For now just clear buffer
-	mov edi, OFFSET screenBuffer
-	mov ecx, SCREEN_WIDTH * SCREEN_HEIGHT
-	mov eax, 0FF000000h
-	rep stosd
-	; TODO: draw rects from commands
-	ret
+    ; Clear the screen buffer
+    mov edi, OFFSET screenBuffer
+    mov ecx, SCREEN_WIDTH * SCREEN_HEIGHT
+    mov eax, 0FF000000h
+    rep stosd
+
+    ; Draw all rect commands using associated transform and color data
+    ; Camera offset is applied when ignoreCamera is not set
+
+    ret
 renderCommands ENDP
 
 END
