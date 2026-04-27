8bb003c946262b59c833fd114371a3d96085f509
Justin Bower <jbower2024@my.fit.edu>
2026-04-27 16:12:28 -0400

    Update tetris_manager.asm
    implementing tetris_manager_update function

--- engine/tetris_manager.asm
+++ engine/tetris_manager.asm
@@ -26,6 +26,25 @@ new_tetris_manager ENDP
 tetris_manager_update PROC stdcall PUBLIC USES esi, deltaTime:REAL4
     mov esi, ecx
     ; Manages active tetromino, spawning, input routing, and game-over conditions
+    mov ebx, (TetrisManager PTR [esi]).activeTetromino
+	.IF ebx == 0
+		fld (TetrisManager PTR [esi]).spawnTimer
+		fadd deltaTime
+		fstp (TetrisManager PTR [esi]).spawnTimer
+		
+		.IF (TetrisManager PTR [esi]).spawnTimer > 0.1
+			INVOKE new_tetromino
+			mov ebx, eax
+			mov ecx, esi
+			mov ecx, (GameObject PTR [ecx]).pParentScene
+			mov ecx, eax
+			INVOKE instantiate_game_object, ebx
+			
+			mov esi, (GameObject PTR [esi]).pParentScene
+			mov (TetrisManager PTR [esi]).activeTetromino, ebx
+			mov (TetrisManager PTR [esi]).spawnTimer, 0.0
+		.ENDIF
+	.ENDIF
     ret
 tetris_manager_update ENDP
 
